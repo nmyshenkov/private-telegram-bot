660e6e175296ba694b4130e630aeb343496d70e1
nmyshenkov <nikita11e@gmail.com>
2022-02-17 13:13:33 +0300

    added bot

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	TargetChatID int64  `json:"target_chat_id"`
+	Token        string `json:"token"`
+	Debug        bool   `json:"debug"`
+}
+
+func (cfg *Config) Validate() error {
+	if cfg.Token == "" {
+		return fmt.Errorf("config: token not present")
+	}
+
+	if cfg.TargetChatID == 0 {
+		return fmt.Errorf("config: target_chat_id not present")
+	}
+
+	return nil
+}
+
+// ParseFromFile читает файл конфигурации с диска и загружает его содержимое
+// в структуру конфига приложения. Поддерживаются файлы в формате YAML и JSON.
+func (cfg *Config) ParseFromFile(name string) error {
+	file, err := ioutil.ReadFile(name)
+	if err != nil {
+		return fmt.Errorf("read file: %w", err)
+	}
+
+	return json.Unmarshal(file, cfg)
+}
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/nmyshenkov/private-telegram-bot/config"
+	"log"
+)
+
+func main() {
+	configPath := flag.String("c", "config_local.json", "config file")
+	flag.Parse()
+
+	cfg := new(config.Config)
+
+	if err := cfg.ParseFromFile(*configPath); err != nil {
+		log.Fatalf("parse config file: %v", err)
+	}
+
+	if err := cfg.Validate(); err != nil {
+		log.Fatalf("validate config file: %v", err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = cfg.Debug
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			if cfg.Debug {
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			}
+
+			msg := tgbotapi.NewMessage(cfg.TargetChatID, update.Message.Text)
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Printf("error send message to channel: %v", err)
+			}
+		}
+	}
+
+}
